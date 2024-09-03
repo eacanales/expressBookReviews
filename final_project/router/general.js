b83df053534b3e1cfc656ea41efbddfd2c3fde02
Your Name <you@example.com>
2024-09-03 10:36:30 -0400

    message

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,11 +4,27 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+// let users = [];
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password; 
+  
+  if (username && password) {
+    if (!doesExist(username)) {
+        users.push({"username": username, "password": password });
+        return res.status(200).json({ message: "User successfully registered. Now you can login" });
+    } else {
+        return res.status(404).json({ message: "User already exist!" });
+    }
+  } 
+  return res.status(404).json({ message: "Unable to register user."});
 });
+
+
+  // return res.status(300).json({message: "Yet to be implemented"});
+//});
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
